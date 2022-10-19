4d58b16ef07535cf5e455894ebcd7128da0a373b
Alejandro <alejandrobrb210501@gmail.com>
2022-10-19 12:56:33 +0200

    (#9) Create a file manager class  * Write and read files from the application documents directory  * Read file from assets

---
+++ sma_user_applicaton/lib/utils/file_manager.dart
@@ -0,0 +1,46 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:flutter/services.dart' show rootBundle;
+
+const String assetsDirectory = 'assets';
+
+
+/// Class that manage the read and write process to a file
+class FileManager {
+  static Future<String> get localPath async {
+    return (await getApplicationDocumentsDirectory()).path;
+  }
+
+  static Future<String> readFileFromAssets(String path) async {
+    return await rootBundle.loadString('$assetsDirectory/$path');
+  }
+
+  static Future<String> readFileFromDocuments(String path) async {
+    final String localPath = await FileManager.localPath;
+    final String filePath = '$localPath/$path';
+
+    return await File(filePath).readAsString();
+  }
+
+  static Future<void> writeFileToDocuments(String path, String content) async {
+    final String localPath = await FileManager.localPath;
+    final String filePath = '$localPath/$path';
+
+    File file = File(filePath);
+
+    /// If the file does not exists, create it
+    if (!(await File(filePath).exists())) {
+      await file.create(recursive: true);
+    }
+
+    await file.writeAsString(content);
+  }
+}
+
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await FileManager.writeFileToDocuments('configurations/devices.json','{"a": "b"}');
+  print(await FileManager.readFileFromDocuments('configurations/devices.json'));
+}
